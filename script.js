e8ed75dc0fc3cd4a5d3b189be3b88adea46ed8db
Mikhail4ch <mihamakarevi426@gmail.com>
2023-10-23 15:12:57 +0300

    Quadratic equation

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ function division() {
     var result = num1 / num2;
     document.getElementById("result").innerHTML = result;
 }
+function solver () {
+    var num3 = parseInt(document.getElementById("num3").value);
+    var num4 = parseInt(document.getElementById("num4").value);
+    var num5 = parseInt(document.getElementById("num5").value);
+    var D = num4*num4 - 4*num3*num5;
+    if (D > 0) {
+    var root1 = (-num4 + Math.sqrt(D)) / (2 * num3);
+    var root2 = (-num4 - Math.sqrt(D)) / (2 * num3);
+    document.getElementById("result2").innerHTML = "The first root:" + " " + root1 + "<br>"+ "The second root:" + " " + root2;
+    }
+   else if (D==0) {
+    var root3 = -num4 / (2*num3);
+     document.getElementById("result2").innerHTML = "The root is:" + " " + root3;
+   }
+   else 
+   document.getElementById("result2").innerHTML = "There are no real roots :(";
+}
